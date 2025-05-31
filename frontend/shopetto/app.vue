35a52bd349aeb01b7a47bac3bb739cd79960fca1
Dylan <immortalizetimes@gmail.com>
2025-05-31 23:02:21 +0200

    frontend - nuxt install

---
+++ frontend/shopetto/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Product from './pages/Product.vue';
+
+</script>
+
+<template>
+  <div>
+    123
+   <Product></Product>
+  </div>
+</template>
---
+++ frontend/shopetto/pages/Product.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold mb-4">Products</h1>
+    <div v-if="pending">Loading...</div>
+    <div v-else>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          {{ product.name }} - ${{ product.price }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: products, pending, error } = await useFetch('http://localhost:8000/api/products/')
+</script>
